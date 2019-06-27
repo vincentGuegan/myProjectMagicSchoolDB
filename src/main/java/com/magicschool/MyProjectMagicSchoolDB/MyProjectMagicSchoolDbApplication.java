11aa2b561f31b73658da485bb124fd99177a8722
Vincent Guegan <gueganvincent@gmail.com>
2019-06-27 19:43:48 +0200

    contrôleur séparé de l'application, retour des champs de la route pour les écoles, résultats filtrés par param country

---
+++ src/main/java/com/magicschool/MyProjectMagicSchoolDB/MyProjectMagicSchoolDbApplication.java
@@ -0,0 +1,13 @@
+package com.magicschool.MyProjectMagicSchoolDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectMagicSchoolDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectMagicSchoolDbApplication.class, args);
+	}
+
+}
